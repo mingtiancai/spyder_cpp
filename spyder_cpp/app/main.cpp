c63079b4731e9d00d1772e2e3f107cd7b3e5aaa3
mingtiancai <2427373908@qq.com>
2023-02-11 16:22:37 +0800

    COMP: switch glags to CLI11

--- spyder_cpp/app/main.cpp
+++ spyder_cpp/app/main.cpp
@@ -6,12 +6,15 @@
 #include "logger.h"
 #include "mainwindow.h"
 
+#include <CLI/CLI.hpp>
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  INFOLOG("setup spyder_cpp!");
-  QApplication a(argc, argv);
-  MainWindow w;
-  w.show();
-  return a.exec();
+	INFOLOG("setup spyder_cpp!");
+
+	QApplication a(argc, argv);
+	MainWindow w;
+	w.show();
+	return a.exec();
 }
