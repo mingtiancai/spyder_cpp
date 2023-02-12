49ea0689343de50b120a465f8dc6f1a741b741f9
mingtiancai <2427373908@qq.com>
2023-02-13 04:41:13 +0800

    STYLE: rename download 3rd part lib cmake file and use main window ico

--- spyder_cpp/app/main.cpp
+++ spyder_cpp/app/main.cpp
@@ -11,16 +11,16 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  INFOLOG("setup spyder_cpp!");
+	INFOLOG("setup spyder_cpp!");
 
-  core::CliOption option;
-  core::getOption(argc, argv, option);
-  INFOLOG(option.print());
+	core::CliOption option;
+	core::getOption(argc, argv, option);
+	INFOLOG(option.print());
 
-  QApplication a(argc, argv);
-  a.setWindowIcon(QIcon(":/spyder.svg"));
-  MainWindow w;
-  w.setWindowTitle("spyder_cpp");
-  w.show();
-  return a.exec();
+	QApplication a(argc, argv);
+	a.setWindowIcon(QIcon(":/windows_app_icon.ico"));
+	MainWindow w;
+	w.setWindowTitle("spyder_cpp");
+	w.show();
+	return a.exec();
 }
