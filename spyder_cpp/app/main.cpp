3ad13974166c76fe4c13ddce9228de78421e1baf
mingtiancai <2427373908@qq.com>
2023-02-13 04:36:55 +0800

    ENH: add all image in spyder_cpp

--- spyder_cpp/app/main.cpp
+++ spyder_cpp/app/main.cpp
@@ -1,28 +1,26 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
 
 #include <QApplication>
 #include <QIcon>
+#include <iostream>
 
-#include <spdlog/spdlog.h>
-
-#include "logger/logger.h"
 #include "cli/cli_option.h"
-
+#include "logger/logger.h"
 #include "mainwindow.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	INFOLOG("setup spyder_cpp!");
+  INFOLOG("setup spyder_cpp!");
 
-	core::CliOption option;
-	core::getOption(argc, argv, option);
-	INFOLOG(option.print());
+  core::CliOption option;
+  core::getOption(argc, argv, option);
+  INFOLOG(option.print());
 
-	QApplication a(argc, argv);
-	a.setWindowIcon(QIcon(":/spyder.ico"));
-	MainWindow w;
-	w.setWindowTitle("spyder_cpp");
-	w.show();
-	return a.exec();
+  QApplication a(argc, argv);
+  a.setWindowIcon(QIcon(":/spyder.svg"));
+  MainWindow w;
+  w.setWindowTitle("spyder_cpp");
+  w.show();
+  return a.exec();
 }
