aa6ffe7b2d442e7c93c10dbe6e7d490a2a5704c7
mingtiancai <2427373908@qq.com>
2023-02-13 04:26:44 +0800

    ENH:add app title

--- spyder_cpp/app/main.cpp
+++ spyder_cpp/app/main.cpp
@@ -22,6 +22,7 @@ int main(int argc, char* argv[]) {
 	QApplication a(argc, argv);
 	a.setWindowIcon(QIcon(":/spyder.ico"));
 	MainWindow w;
+	w.setWindowTitle("spyder_cpp");
 	w.show();
 	return a.exec();
 }
